8dc006f8d9da1c6e158075b0115cedcdc51a97f5
StevenChen <949727157@qq.com>
2019-11-16 15:26:07 +0800

    update

--- study/testFashionMinst.py
+++ study/testFashionMinst.py
@@ -2,7 +2,7 @@
 import os
 
 import tensorflow as tf
-from tensorflow.keras import datasets, Sequential, layers, optimizers
+from tensorflow.keras import datasets, Sequential, layers, optimizers, metrics
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -18,6 +18,7 @@ def preprocess(x, y):
 batchSize = 128
 
 db = tf.data.Dataset.from_tensor_slices((x, y))
+# map 数据预处理, shuffle 数据打乱, batch 一次读取多少数据
 db = db.map(preprocess).shuffle(10000).batch(batchSize)
 
 db_test = tf.data.Dataset.from_tensor_slices((x_test, y_test))
@@ -26,10 +27,15 @@ db_test = db_test.map(preprocess).batch(batchSize)
 inputShape = 28 * 28
 
 model = Sequential([
+    layers.Dense(768, activation=tf.nn.relu),
     layers.Dense(512, activation=tf.nn.relu),
+    layers.Dense(384, activation=tf.nn.relu),
     layers.Dense(256, activation=tf.nn.relu),
+    layers.Dense(192, activation=tf.nn.relu),
     layers.Dense(128, activation=tf.nn.relu),
+    layers.Dense(96, activation=tf.nn.relu),
     layers.Dense(64, activation=tf.nn.relu),
+    layers.Dense(42, activation=tf.nn.relu),
     layers.Dense(32, activation=tf.nn.relu),
     layers.Dense(10)
 ])
@@ -37,6 +43,9 @@ model.build(input_shape=[None, inputShape])
 model.summary()
 
 optimizer = optimizers.Adam(lr=1e-3)
+
+lossMean = metrics.Mean()
+accuracy = metrics.Accuracy()
 
 
 def main():
@@ -48,26 +57,24 @@ def main():
                 logits = model(x)
                 loss = tf.losses.categorical_crossentropy(y_onehot, logits, from_logits=True)
                 loss = tf.reduce_sum(loss)
+                lossMean.update_state(loss)
 
             gradient = tape.gradient(loss, model.trainable_variables)
             optimizer.apply_gradients(zip(gradient, model.trainable_variables))
 
             if step % 100 == 0:
-                print(epoch, step, "train loss:", float(loss))
+                print(epoch, step, "train loss:", lossMean.result().numpy())
+                lossMean.reset_states()
 
-        correct_num = 0
-        correct_total = 0
+        accuracy.reset_states()
         for (x, y) in db_test:
             x = tf.reshape(x, [-1, inputShape])
             logits = model(x)
             pro = tf.nn.softmax(logits, axis=1)
             pro = tf.cast(tf.argmax(pro, axis=1), dtype=tf.int32)
-            correct = tf.reduce_sum(tf.cast(tf.equal(pro, y), dtype=tf.int32))
+            accuracy.update_state(y, pro)
 
-            correct_num += x.shape[0]
-            correct_total += int(correct)
-
-        print(epoch, "test correct:", correct_total / correct_num)
+        print(epoch, "test correct:", accuracy.result().numpy())
 
 
 if __name__ == '__main__':
