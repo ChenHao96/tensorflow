7681772da0aa764ecae2f0012f8369f6d83b0bbe
StevenChen <949727157@qq.com>
2019-12-26 15:14:56 +0800

    update cnn

--- study/testCNN.py
+++ study/testCNN.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 from tensorflow.keras import datasets, Sequential, layers, metrics, optimizers
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
+tf.random.set_seed(2345)
 
 def preprocess(x, y):
     x = 2 * tf.cast(x, dtype=tf.float32) / 255. - 1.
@@ -13,18 +13,18 @@ def preprocess(x, y):
     return x, y
 
 
-batchSize = 128
 (x, y), (val_x, val_y) = datasets.cifar100.load_data()
 
-y = tf.one_hot(y, depth=100)
-y = tf.squeeze(y)
-val_y = tf.squeeze(val_y)
+y = tf.squeeze(y, axis=1)
+val_y = tf.squeeze(val_y, axis=1)
 
 train_db = tf.data.Dataset.from_tensor_slices((x, y))
-train_db = train_db.map(preprocess).shuffle(20000).batch(batchSize)
+train_db = train_db.map(preprocess).shuffle(10000).batch(250)
 
 val_db = tf.data.Dataset.from_tensor_slices((val_x, val_y))
-val_db = val_db.map(preprocess).batch(batchSize)
+val_db = val_db.map(preprocess).batch(100)
+
+fc_input_shape = 512
 
 cnn_net = Sequential([
     layers.Conv2D(64, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
@@ -39,18 +39,17 @@ cnn_net = Sequential([
     layers.Conv2D(512, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
     layers.Conv2D(512, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
     layers.MaxPool2D(pool_size=[2, 2], strides=2, padding='same'),
-    layers.Conv2D(512, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
-    layers.Conv2D(512, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
+    layers.Conv2D(fc_input_shape, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
+    layers.Conv2D(fc_input_shape, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
     layers.MaxPool2D(pool_size=[2, 2], strides=2, padding='same')
 ])
 cnn_net.build(input_shape=[None, 32, 32, 3])
 cnn_net.summary()
 
-fc_input_shape = 512
 fc_net = Sequential([
     layers.Dense(256, activation=tf.nn.relu),
     layers.Dense(128, activation=tf.nn.relu),
-    layers.Dense(100)
+    layers.Dense(100, activation=None)
 ])
 fc_net.build(input_shape=[None, fc_input_shape])
 fc_net.summary()
@@ -61,24 +60,25 @@ accuracy = metrics.Accuracy()
 optimizer = optimizers.Adam(lr=3.1415926e-4)
 variables = cnn_net.trainable_variables + fc_net.trainable_variables
 
-# cnn_net.load_weights("checkpoint/testCNN.cnn")
-# fc_net.load_weights("checkpoint/testCNN.fc")
+cnn_net.load_weights("checkpoint/testCNN.cnn")
+fc_net.load_weights("checkpoint/testCNN.fc")
 
 history_acc = 0
-for epoch in range(100):
+for epoch in range(20):
 
     for step, (x, y) in enumerate(train_db):
         with tf.GradientTape() as tape:
             out = cnn_net(x)
             out = tf.reshape(out, (-1, fc_input_shape))
             logits = fc_net(out)
+            y = tf.one_hot(y, depth=100)
             loss = tf.losses.categorical_crossentropy(y, logits, from_logits=True)
             loss = tf.reduce_mean(loss)
             lossMean.update_state(loss)
         gradient = tape.gradient(loss, variables)
         optimizer.apply_gradients(zip(gradient, variables))
 
-        if step % 100 == 0:
+        if step % 10 == 0:
             print(epoch, step, "train loss:", lossMean.result().numpy())
             lossMean.reset_states()
 
