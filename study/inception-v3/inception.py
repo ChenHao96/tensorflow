db4fc3b42d9db51d235af76240ce985c0806aa09
StevenChen <949727157@qq.com>
2020-01-04 19:08:34 +0800

    add inceptionV3

---
+++ study/inception-v3/inception.py
@@ -0,0 +1,152 @@
+# use tensorflow-2.0
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, Sequential
+
+
+class Figure5(layers.Layer):
+    def __init__(self):
+        super(Figure5, self).__init__()
+
+        self.branches1 = Sequential([
+            layers.Conv2D(128, kernel_size=(1, 1), strides=1),
+            layers.Conv2D(128, kernel_size=(3, 3), strides=1, padding="same"),
+            layers.Conv2D(192, kernel_size=(3, 3), strides=1, padding="same")
+        ])
+
+        self.branches2 = Sequential([
+            layers.Conv2D(128, kernel_size=(1, 1), strides=1),
+            layers.Conv2D(192, kernel_size=(3, 3), strides=1, padding="same"),
+        ])
+
+        self.branches3 = Sequential([
+            layers.MaxPool2D(pool_size=(3, 3), strides=(1, 1), padding="same"),
+            layers.Conv2D(192, kernel_size=(1, 1), strides=1)
+        ])
+
+        self.branches4 = layers.Conv2D(192, kernel_size=(1, 1), strides=1)
+        pass
+
+    def call(self, inputs, training=None):
+        return tf.concat([
+            self.branches1(inputs),
+            self.branches2(inputs),
+            self.branches3(inputs),
+            self.branches4(inputs)], 3)
+
+
+class Figure6(layers.Layer):
+    def __init__(self):
+        super(Figure6, self).__init__()
+
+        self.branches1 = Sequential([
+            layers.Conv2D(256, kernel_size=(1, 1), strides=1),
+            layers.Conv2D(256, kernel_size=(1, 7), strides=1, padding="same"),
+            layers.Conv2D(256, kernel_size=(7, 1), strides=1, padding="same"),
+            layers.Conv2D(320, kernel_size=(1, 7), strides=1, padding="same"),
+            layers.Conv2D(320, kernel_size=(7, 1), strides=1, padding="same")
+        ])
+
+        self.branches2 = Sequential([
+            layers.Conv2D(256, kernel_size=(1, 1), strides=1),
+            layers.Conv2D(320, kernel_size=(1, 7), strides=1, padding="same"),
+            layers.Conv2D(320, kernel_size=(7, 1), strides=1, padding="same")
+        ])
+
+        self.branches3 = Sequential([
+            layers.MaxPool2D(pool_size=(3, 3), strides=(1, 1), padding="same"),
+            layers.Conv2D(320, kernel_size=(1, 1), strides=1)
+        ])
+
+        self.branches4 = layers.Conv2D(320, kernel_size=(1, 1), strides=1)
+        pass
+
+    def call(self, inputs, training=None):
+        return tf.concat([
+            self.branches1(inputs),
+            self.branches2(inputs),
+            self.branches3(inputs),
+            self.branches4(inputs)], 3)
+
+
+class Figure7(layers.Layer):
+    def __init__(self):
+        super(Figure7, self).__init__()
+
+        self.branches1 = Sequential([
+            layers.Conv2D(256, kernel_size=(1, 1), strides=1),
+            layers.Conv2D(512, kernel_size=(3, 3), strides=1, padding="same")
+        ])
+        self.branches1_1 = layers.Conv2D(256, kernel_size=(1, 3), strides=1, padding="same")
+        self.branches1_2 = layers.Conv2D(256, kernel_size=(3, 1), strides=1, padding="same")
+
+        self.branches2 = layers.Conv2D(512, kernel_size=(1, 1), strides=1)
+        self.branches2_1 = layers.Conv2D(256, kernel_size=(1, 3), strides=1, padding="same")
+        self.branches2_1 = layers.Conv2D(256, kernel_size=(3, 1), strides=1, padding="same")
+
+        self.branches3 = Sequential([
+            layers.MaxPool2D(pool_size=(3, 3), strides=(1, 1), padding="same"),
+            layers.Conv2D(512, kernel_size=(1, 1), strides=1)
+        ])
+
+        self.branches4 = layers.Conv2D(512, kernel_size=(1, 1), strides=1)
+        pass
+
+    def call(self, inputs, training=None):
+        out1 = self.branches1(inputs)
+        out2 = self.branches2(inputs)
+        return tf.concat([
+            self.branches1_1(out1),
+            self.branches1_2(out1),
+            self.branches2_1(out2),
+            self.branches2_2(out2),
+            self.branches3(inputs),
+            self.branches4(inputs)], 3)
+
+
+class InceptionV3(keras.Model):
+    def __init__(self, classes_num):
+        super(InceptionV3, self).__init__()
+
+        self.stem = Sequential([
+            layers.Conv2D(32, kernel_size=(3, 3), strides=(2, 2)),
+            layers.Conv2D(32, kernel_size=(3, 3), strides=(1, 1)),
+            layers.Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding="same"),
+            layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
+            layers.Conv2D(80, kernel_size=(3, 3), strides=(1, 1)),
+            layers.Conv2D(192, kernel_size=(3, 3), strides=(2, 2)),
+            layers.Conv2D(228, kernel_size=(3, 3), strides=(1, 1))
+        ])
+
+        self.figure5 = Sequential([
+            Figure5(),
+            Figure5(),
+            Figure5()
+        ])
+
+        self.figure6 = Sequential([
+            Figure6(),
+            Figure6(),
+            Figure6(),
+            Figure6(),
+            Figure6()
+        ])
+
+        self.figure7 = Sequential([
+            Figure7(),
+            Figure7()
+        ])
+
+        self.avgPool = layers.GlobalAveragePooling2D()
+        self.fc = layers.Dense(classes_num)
+
+        pass
+
+    def call(self, inputs, training=None):
+        out = self.stem(inputs)
+        out = self.figure5(out)
+        out = self.figure6(out)
+        out = self.figure7(out)
+        out = self.avgPool(out)
+        return self.fc(out)
---
+++ study/inception-v3/trainInceptionV3ByFit.py
@@ -0,0 +1,44 @@
+# use tensorflow-2.0
+import os
+
+import tensorflow as tf
+from inception import InceptionV3
+from tensorflow.keras import datasets, optimizers
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+tf.random.set_seed(2345)
+
+
+def preprocess(x, y):
+    x = 2 * tf.cast(x, dtype=tf.float32) / 255. - 1.
+    y = tf.cast(y, dtype=tf.int32)
+    y = tf.one_hot(y, depth=classes)
+    return x, y
+
+
+classes = 100
+(x, y), (val_x, val_y) = datasets.cifar100.load_data()
+
+input_shape = (None, 32, 32, 3)
+y = tf.squeeze(y, axis=1)
+val_y = tf.squeeze(val_y, axis=1)
+
+train_db = tf.data.Dataset.from_tensor_slices((x, y))
+train_db = train_db.map(preprocess).shuffle(10000).batch(250)
+
+val_db = tf.data.Dataset.from_tensor_slices((val_x, val_y))
+val_db = val_db.map(preprocess).batch(250)
+
+model = InceptionV3(classes)
+model.build(input_shape=input_shape)
+model.summary()
+
+optimizer = optimizers.Adam(lr=3.1415926e-4)
+
+model.compile(optimizer=optimizer,
+              loss=tf.losses.CategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_db, epochs=20, validation_data=val_db, validation_steps=1)
+
+model.evaluate(val_db)
